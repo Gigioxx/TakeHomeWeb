09752a5a37747c3da444ae4eabfd5872837663bf
Gigioxx <guillermo.casanova.b@gmail.com>
2025-08-29 11:35:51 -0400

    chore: configure build tooling

--- birds-vite/vite.config.ts
+++ birds-vite/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
-		plugins: [react()],
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
 		server: {
 			proxy: {
 				'/api/graphql': env.GRAPHQL_ENDPOINT,
